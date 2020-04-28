c8d0f44c35440319901bdd52eefeead0887365c6
Seekwhence <reakisak@protonmail.com>
2020-04-28 16:12:00 +0200

    added contanct info in the help guide

--- src/components/help/Contact.js
+++ src/components/help/Contact.js
@@ -4,7 +4,18 @@ const Contact = () => {
   return (
     <div>
       <h3 className="title">Contact info</h3>
-      <p>some details</p>
+      <p>
+        I hope the Help Guide explained all of the things Kanjidex can do
+        efficiently. If in any case you think there was something left out or if
+        something's not clear, or worse yet, a bug, please, let me know at{" "}
+        <strong>hello@kanjidex.com</strong>
+      </p>
+      <p>
+        Likewise, feel free to give any sort of feedback or even just say hi. I
+        will usually respond within two days unless I'm somewhere on the road
+        (hopefully, somewhere in Japan) in which case it might be three or four
+        days.
+      </p>
     </div>
   );
 };
