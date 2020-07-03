14ac2eedc5a7cb1d45fb40a08f6d706694b3f481
Seekwhence <reakisak@protonmail.com>
2020-07-03 17:35:27 +0200

    minor fixes for the tooltip/quicksearch

--- src/content.js
+++ src/content.js
@@ -1,11 +1,7 @@
 /* global chrome */
 import tippy from "tippy.js";
-const uuid = require("uuid/v4");
 
 function showInfo(kanji) {
-  // create unique id for the target element so that tippy can display the relevant info
-  const uniqueId = uuid();
-  const targetId = `kanjidex-${uniqueId}`;
   // get the kanji character
   const regex = new RegExp(`(${kanji.query})`);
   const selection = window.getSelection();
@@ -20,7 +16,7 @@ function showInfo(kanji) {
 
     // return the matching kanji with a wrapped span and the template with the basic info about the kanji
     if (part.match(regex)) {
-      return `<span class="kanjidex-tooltip ${targetId}" data-tippy-content="<div class='row'><div class='main-info'><p class='kanjidex-title'><strong>Meaning: </strong> ${
+      return `<span class="kanjidex-tooltip" data-tippy-content="<div class='row'><div class='main-info'><p class='kanjidex-title'><strong>Meaning: </strong> ${
         kanji.meaning
       }</p><p class='kanjidex-info'><strong>Onyomi: </strong>${
         kanji.onyomi.length > 0 ? kanji.onyomi : "not found"
@@ -28,9 +24,7 @@ function showInfo(kanji) {
         kanji.kunyomi.length > 0 ? kanji.kunyomi : "not found"
       }</p><p class='kanjidex-info'><img src='//images.weserv.nl/?url=${
         kanji.strokeOrderDiagramUri
-      }' alt='${kanji.meaning}' /></p><p class='kanjidex-info'><a href='${
-        kanji.uri
-      }' target='_blank'>View on jisho.org</a></p></div></div>">${part}</span>`;
+      }' alt='${kanji.meaning}' /></p></div></div>">${part}</span>`;
     }
 
     return part;
@@ -43,8 +37,11 @@ function showInfo(kanji) {
   tippy("[data-tippy-content]", {
     allowHTML: true,
     theme: "kanjidex",
-    interactive: true,
+    maxWidth: 450,
+    placement: "auto",
+    role: "tooltip",
   });
+  console.log(kanji);
 }
 
 // when Kanjidex gets message from background about the highlighted kanji
