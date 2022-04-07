f5511a41504b72df4087a8acfb7e17aa89bffdb8
kirea <reakisak@protonmail.com>
2022-04-07 11:55:42 +0200

    fixed vocab context

--- src/components/Vocabulary.js
+++ src/components/Vocabulary.js
@@ -1,19 +1,16 @@
 import React, { useState } from "react";
 import NewDeckForm from "./vocabulary/NewDeckForm";
-import VocabularyContextProvider from "../contexts/VocabContext";
 import VocabularyList from "./vocabulary/VocabularyList";
 
 const Vocabulary = () => {
   const [mood, setMood] = useState("happy");
   return (
-    <VocabularyContextProvider>
-      <div className="Vocabulary">
-        <div className="container">
-          <NewDeckForm setMood={setMood} />
-          <VocabularyList mood={mood} />
-        </div>
+    <div className="Vocabulary">
+      <div className="container">
+        <NewDeckForm setMood={setMood} />
+        <VocabularyList mood={mood} />
       </div>
-    </VocabularyContextProvider>
+    </div>
   );
 };
 
